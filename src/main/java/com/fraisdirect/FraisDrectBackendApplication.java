0dc04e0ac361464fe86ec5166b920188d92c50d7
Arona Ndiaye <arona10ndiaye@gmail.com>
2024-06-25 11:01:08 +0000

    initial commit

---
+++ src/main/java/com/fraisdirect/FraisDrectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fraisdirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraisDrectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraisDrectBackendApplication.class, args);
+	}
+
+}
